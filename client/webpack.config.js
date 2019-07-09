518a83b291e5fa1f64d82cb87ae1924735c38c24
GabrieleSuzart <gabrielesuzart@gmail.com>
2019-07-09 14:47:31 -0300

    Minimum webpack configuration

---
+++ client/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path') // pegar o caminho absoluto
+
+module.exports = {
+    entry: './app-src/app.js', // primeiro modulo que será carregado na minha aplicação
+    output: { // configuraçao do arquivo de bundle gerado no final
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist') // __dirname é uma variável do JS que pega o diretório atual
+    }
+}
